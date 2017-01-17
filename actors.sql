1a7683575d1fd7c773f8d13990ecf7fcf76f8aa8
zbordofsky <zacharybordofsky@gmail.com>
2017-01-17 13:43:15 -0800

    144 first commit

---
+++ actors.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE Actors(Name VARCHAR(40), Movie VARCHAR(80), Year INTEGER, Role VARCHAR(40)); 
+LOAD DATA LOCAL INFILE '~/data/actors.csv' INTO TABLE Actors FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'; 
+SELECT Name FROM Actors WHERE Movie = 'Die Another Day'; 
+DROP TABLE Actors; 
