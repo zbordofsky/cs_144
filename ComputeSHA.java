7f9fc0345f970cacd0cbd3609e89ecfe102a3090
Kyle Haacker <HAACKERK@HAACKERK-2.local>
2017-01-18 11:47:09 -0800

    fixed computeSHA

--- ComputeSHA.java
+++ ComputeSHA.java
@@ -3,47 +3,39 @@ import java.security.*;
 
 public class ComputeSHA
 {
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException
-	{
-		FileInputStream inFile = new FileInputStream(args[0]);
-		MessageDigest shaHash = MessageDigest.getInstance("SHA-1"); 
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
-		byte[] byteArray = new byte[1024];
+		if (args.length != 1) {
+			System.err.println("error: " + args.length + " arguments supplied, expected 1");
+			System.err.println("usage: java ComputeSHA inputfile");
+			System.exit(-1);
+		}
+
+		FileInputStream inputFile = new FileInputStream(args[0]);
+		MessageDigest shaHash = MessageDigest.getInstance("SHA-1");
+
+		byte[] byteArray = new byte[512];
 		int bytesRead = -1;
 
-		// todo: use for loop
-		while ((bytesRead = inFile.read(byteArray)) != -1) {
+		while ((bytesRead = inputFile.read(byteArray)) != -1) {
 			shaHash.update(byteArray, 0, bytesRead);
 		}
 
-		//byte[] result = shaHash.digest();
-		String result = shaHash.digest().toString();
-		System.out.println(result);
-
-		inFile.close();
-/*
-		//FileInputStream inFile = null;
-		//char ch;
-
-		try
-		{
-		    inFile = new FileInputStream(args[0]);
-
-		    while ((ch = inFile.read()) != -1) 
-		    {
-			System.out.println(ch);
-		    }
-		}
-		finally
-		{
-		    if (inFile != null)
-		    {
-			    inFile.close();
-		    }
+		inputFile.close();
+		byte[] byteDigest = shaHash.digest();
+		
+		StringBuilder hexDigest = new StringBuilder();
+		for (int i = 0; i < byteDigest.length; i++) {
+			hexDigest.append(String.format("%02x", byteDigest[i]));
 		}
 
-		inFile.close();
-	}
-*/
+		System.out.println(hexDigest.toString());
+
+		//StringBuilder hexDigest = new StringBuilder();
+		//for (int i = 0; i < byteDigest.length; i++) {
+		//	hexDigest.append(Integer.toString( (byteDigest[i] & 0xFF) + 0x100, 16 ).substring(1));
+		//}
+
+		//System.out.println(hexDigest);
 	}
 }
