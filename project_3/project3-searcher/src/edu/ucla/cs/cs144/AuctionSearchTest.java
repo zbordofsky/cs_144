a099737b78da7a36a3e8daa7a184ec205f213463
Kyle Haacker <HAACKERK@HAACKERK-2.local>
2017-02-16 23:39:46 -0800

    finish project 3

--- project_3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ project_3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -16,7 +16,7 @@ public class AuctionSearchTest {
 		String reply = as.echo(message);
 		System.out.println("Reply: " + reply);
 		
-		String query = "star trek";
+		String query = "superman";
 		SearchResult[] basicResults = as.basicSearch(query, 0, 20);
 		System.out.println("Basic Seacrh Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");
@@ -33,10 +33,7 @@ public class AuctionSearchTest {
 			System.out.println(result.getItemId() + ": " + result.getName());
 		}
 		
-		//String itemId = "1497595357";
-		String itemId = "1045767117";
-		//String itemId = "1044995614";
-		//String itemId = "1043374545";
+		String itemId = "1497595357";
 		String item = as.getXMLDataForItemId(itemId);
 		System.out.println("XML data for ItemId: " + itemId);
 		System.out.println(item);
