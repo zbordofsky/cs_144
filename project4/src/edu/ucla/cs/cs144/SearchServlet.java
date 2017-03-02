81cddc2f399fb3938571773d902180217636d043
Kyle Haacker <HAACKERK@HAACKERK-2.local>
2017-03-01 16:24:46 -0800

    wednesday

--- project4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ project4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -13,6 +13,23 @@ public class SearchServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        // Obtain user search input. If page has loaded for first time, value will be 'null'
+        String query = request.getParameter("query");
+
+        // First time search page has loaded, or user did not specify a query to search by
+        if (query == null || query.isEmpty()) {
+            response.sendRedirect("/eBay/auctionSearch.html");
+            return;
+        }
+
+        /*if (query != null && !query.isEmpty()) {
+        	;
+
+        	//SearchResult[] searchResults = AuctionSearch.basicSearch(query, 0, 10);
+        	//request.setAttribute("searchResults", searchResults);
+        }*/
+
+        //request.setAttribute("query", query);
+        //request.getRequestDispatcher("/search.jsp").forward(request, response);
     }
 }
