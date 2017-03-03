e2266debcb23ef23a7ed6355dc6318766c91bea2
Kyle Haacker <HAACKERK@HAACKERK-2.local>
2017-03-02 22:08:36 -0800

    cache me inside

--- project4/WebContents/SuggestionProvider.js
+++ project4/WebContents/SuggestionProvider.js
@@ -28,19 +28,31 @@ function SuggestionProvider() {
  */
 SuggestionProvider.prototype.requestSuggestions = function (oAutoSuggestControl /*:AutoSuggestControl*/,
                                                           bTypeAhead /*:boolean*/) {
-    var aSuggestions = [];
-    var sTextboxValue = oAutoSuggestControl.textbox.value;
     
-    if (sTextboxValue.length > 0){
-    
-        //search for matching states
-        for (var i=0; i < this.states.length; i++) { 
-            if (this.states[i].indexOf(sTextboxValue) == 0) {
-                aSuggestions.push(this.states[i]);
-            } 
-        }
-    }
+    var that = oAutoSuggestControl;
+	var aSuggestions = [];
+	var sTextboxValue = oAutoSuggestControl.textbox.value;
 
-    //provide suggestions to the control
-    oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+	// search for suggestions if input text box is non-empty
+	if (sTextboxValue.length > 0) {
+		var xmlHttp = new XMLHttpRequest();
+		var request = "suggest?query=" + encodeURI(sTextboxValue);
+		xmlHttp.open("GET", request);
+		xmlHttp.onreadystatechance = function() {
+			if (xmlHttp.readyState == 4) {
+				var response = xmlHttp.responseText;
+				var xmlDom = new DOMParser().parseFromString(response, "text/xml");
+				var suggestions = xmlDom.getElementsByTagName("suggestion");
+				var currSuggestion;
+				for (var i = 0; i < suggestions.length; i++) {
+					currSuggestion = suggestions[i].getAttribute("data");
+					aSuggestions.push(currSuggestion);
+				}
+
+				oAutoSuggestControl.autosuggest(aSuggestions, bTypeAhead);
+			}
+		};
+
+		xmlHttp.send(null);
+	};
 };
